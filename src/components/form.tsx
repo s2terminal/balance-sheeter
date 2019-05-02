8260a3dc68b733be82724d04823bcf0ff9b99462
suzuki.sh <suzuki.sh@s2terminal.com>
2019-05-02 19:30:21 +0900

    save query string

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -32,30 +32,58 @@ class BalanceSheetForm extends React.Component<Props, State> {
     equity: null,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = this.loadQueryString();
+  }
+
   handleChange = (name: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
     this.setState({ [name]: event.target.value } as Pick<State, keyof State>);
   };
+  handleBlur = () => {
+    this.saveQueryString(this.state);
+  }
+
+  saveQueryString = (state: State) => {
+    const url = new URL(window.location.href);
+    url.searchParams.set("bs", JSON.stringify(state));
+    window.history.pushState(null, null, url.toString());
+  }
+
+  loadQueryString() {
+    const url = new URL(window.location.href);
+    return JSON.parse(url.searchParams.get("bs"));
+  }
+
+
+  generateTextField = (elem: BSItem) => {
+    const val = this.state[elem.name] ? this.state[elem.name] : '';
+    return <TextField
+      id={`bs-${elem.name}`}
+      key={`bs-${elem.name}`}
+      label={`${elem.label}`}
+      value={val}
+      onChange={this.handleChange(elem.name)}
+      onBlur={this.handleBlur}
+    />
+  }
 
   render() {
-    const generateTextField = (elem: BSItem) => {
-      const val = this.state[elem.name] ? this.state[elem.name] : '';
-
-      return <TextField
-        id={`bs-${elem.name}`}
-        key={`bs-${elem.name}`}
-        label={`${elem.label}`}
-        value={val}
-        onChange={this.handleChange(elem.name)}
-      />
-    }
-
     const assetFields = assetItems.map(function(elem) {
-      return generateTextField(elem);
+      return this.generateTextField(elem);
     }, this);
 
     const liabilityFields = liabilityItems.map(function(elem) {
-      return generateTextField(elem);
+      return this.generateTextField(elem);
     }, this);
+
+
+    const assetBars = assetItems.map(function(elem) {
+      return <Bar stackId="a" key={elem.name} dataKey={elem.name} name={elem.label} fill="#333" stroke="#ccc" />;
+    });
+    const liabilityBars = liabilityItems.map(function(elem) {
+      return <Bar stackId="b" key={elem.name} dataKey={elem.name} name={elem.label} fill="#333" stroke="#ccc" />;
+    });
 
     const data = [
       {
@@ -83,12 +111,14 @@ class BalanceSheetForm extends React.Component<Props, State> {
           <YAxis/>
           <Tooltip/>
           <Legend />
-          <Bar stackId="a" dataKey="cash" fill="#8884d8" />
+          {/* <Bar stackId="a" dataKey="cash" fill="#8884d8" />
           <Bar stackId="a" dataKey="currentAssets" fill="#82ca9d" />
           <Bar stackId="a" dataKey="nonCurrentAssets" fill="#ffc658"/>
           <Bar stackId="b" dataKey="currentLiabilities" fill="#82ca9d" />
           <Bar stackId="b" dataKey="nonCurrentLiabilities" fill="#ffc658"/>
-          <Bar stackId="b" dataKey="equity" fill="#8884d8" />
+          <Bar stackId="b" dataKey="equity" fill="#8884d8" /> */}
+          {assetBars}
+          {liabilityBars}
         </BarChart>
       </>
     );
