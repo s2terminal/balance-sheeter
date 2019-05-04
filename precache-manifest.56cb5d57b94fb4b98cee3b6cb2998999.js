399cc0fd85ac7aefd11baf2f5d5ba65218bf2b31
suzuki.sh <suzuki.sh@s2terminal.com>
2019-05-04 20:48:07 +0900

    Updates

--- precache-manifest.56cb5d57b94fb4b98cee3b6cb2998999.js
+++ precache-manifest.56cb5d57b94fb4b98cee3b6cb2998999.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b38ebdba6ceeea2374c391289a466b26",
+    "revision": "11b489439af9f6c3cb919e9e70be85ac",
     "url": "/balance-sheeter/index.html"
   },
   {
-    "revision": "1a0655ff21d4f230dd7b",
+    "revision": "9816cd6493a226340689",
     "url": "/balance-sheeter/static/css/main.42cc8b22.chunk.css"
   },
   {
-    "revision": "00481e11f5c9c12bfc0f",
-    "url": "/balance-sheeter/static/js/2.c38d5512.chunk.js"
+    "revision": "16e6010fe27b8250e3bf",
+    "url": "/balance-sheeter/static/js/2.88005a08.chunk.js"
   },
   {
-    "revision": "1a0655ff21d4f230dd7b",
-    "url": "/balance-sheeter/static/js/main.8399c2c6.chunk.js"
+    "revision": "9816cd6493a226340689",
+    "url": "/balance-sheeter/static/js/main.2b532a7d.chunk.js"
   },
   {
     "revision": "e66a9d4df8a94e019b54",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/balance-sheeter/precache-manifest.9321684fe3a61fb387f82377961739d5.js"
+  "/balance-sheeter/precache-manifest.56cb5d57b94fb4b98cee3b6cb2998999.js"
 );
 
 self.addEventListener('message', (event) => {
